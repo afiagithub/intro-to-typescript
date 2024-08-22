746a0069ed95a9b510eeae95ee7cd56737bd72e7
Afia Rahman <rahmanmou26@gmail.com>
2024-08-22 22:24:33 +0600

    readonly use case

--- objects/object-type.js
+++ objects/object-type.js
@@ -1,11 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function createUser(_a) {
     var string = _a.name, number = _a.age;
     return 'Added';
 }
-// if the function takes an object and also return an object
+// if the function return an object
 function addUser() {
     return { name: 'Yash', age: 25 };
 }
 var newUser = { name: 'Akash', age: 21 };
 console.log(createUser(newUser));
 console.log(addUser());
+function addCourse(crs) {
+    return { title: '', duration: 5, price: 99.45 };
+}
+console.log(addCourse({ title: '', duration: 5, price: 99.45 }));
+var newPr = {
+    // readonly is used when you don't want someone to change the value of that field.
+    _id: ['1', '2'],
+    name: 'milkvita',
+    price: 45.50,
+    quantity: 125
+};
+newPr.price = 47;
+// newPr._id.push('6')
+console.log(newPr);
--- objects/object-type.ts
+++ objects/object-type.ts
@@ -1,13 +1,13 @@
-function createUser({name: string, age: number}){
+function createUser({ name: string, age: number }) {
     return 'Added'
 }
 
 // if the function return an object
-function addUser():{name: string, age: number}{
-    return {name: 'Yash', age: 25}
+function addUser(): { name: string, age: number } {
+    return { name: 'Yash', age: 25 }
 }
 
-let newUser = {name: 'Akash', age: 21}
+let newUser = { name: 'Akash', age: 21 }
 
 console.log(createUser(newUser));
 console.log(addUser());
@@ -19,11 +19,39 @@ type Course = {
     price: number;
 }
 
-function addCourse(crs: Course): Course{
-    return {title: '', duration: 5, price: 99.45}
+function addCourse(crs: Course): Course {
+    return { title: '', duration: 5, price: 99.45 }
 }
 
-console.log(addCourse({title: '', duration: 5, price: 99.45}));
+console.log(addCourse({ title: '', duration: 5, price: 99.45 }));
+
+type Product = {
+    // readonly is used when you don't want someone to change the value of that field.
+    _id: readonly string[];
+    name: string;
+    price: number;
+    quantity: number
+}
+
+let newPr: Product = {
+    // readonly is used when you don't want someone to change the value of that field.
+    _id: ['1', '2'],
+    name: 'milkvita',
+    price: 45.50,
+    quantity: 125
+}
+
+newPr.price = 47
+// newPr._id.push('6')
+console.log(newPr);
+
+type transaction = {
+    tran_id: string
+}
+
+type fullProduct = Product & transaction & {
+    hasDiscount: boolean
+}
 
 
-export {}
+export { }
