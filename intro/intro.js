a32c044f53e9c58836b6cbd0b7b80699d10696ca
Afia Rahman <rahmanmou26@gmail.com>
2024-08-22 16:26:29 +0600

    used primitive type

--- intro/intro.js
+++ intro/intro.js
@@ -1,2 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// string
 var full_name = "Afia Rahman";
+full_name.toUpperCase();
 console.log(full_name);
+// number
+var userId = 47;
+var priceFood = 42.5;
+console.log(userId, priceFood);
+// boolean
+var doesExist = false;
+console.log(doesExist);
+// any
+var checkVal;
+checkVal = 'hi';
+console.log(checkVal);
+var checkAny = 90;
+checkAny += 10;
+console.log(checkAny);
--- intro/intro.ts
+++ intro/intro.ts
@@ -1,5 +1,26 @@
+// string
 let full_name: string = "Afia Rahman";
-
+full_name.toUpperCase()
 console.log(full_name);
+
+// number
+let userId: number = 47;
+let priceFood = 42.5
+console.log(userId, priceFood);
+
+// boolean
+let doesExist: boolean = false;
+console.log(doesExist);
+
+// any
+let checkVal;
+checkVal = 'hi'
+console.log(checkVal);
+
+let checkAny: any = 90;
+checkAny += 10
+console.log(checkAny);
+
+
 
 export {}
