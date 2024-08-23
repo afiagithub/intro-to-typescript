905a4164f4a2fbeee4b4522ac887df1d36bb3569
Afia Rahman <rahmanmou26@gmail.com>
2024-08-23 13:56:36 +0600

    used enum type

---
+++ enums/enum.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var seatAllot = 2 /* SeatChoice.Middle */;
+console.log(seatAllot);
---
+++ enums/enum.ts
@@ -0,0 +1,11 @@
+const enum SeatChoice {
+    Aisle = 1,
+    Middle,
+    Window,
+    Fourth
+}
+
+let seatAllot = SeatChoice.Middle
+console.log(seatAllot);
+
+export {}
